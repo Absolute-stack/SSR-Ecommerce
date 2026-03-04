4125a847dd974204c81faacd214d52fbf5890379
Absolute-stack <imdeabsolute@gmail.com>
2026-03-04 15:21:04 +0000

    hitter

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -1,5 +1,6 @@
 import { Order } from "../models/orderModel.js";
 import { Product } from "../models/productModel.js";
+import { checkAndDeactivate } from "./productController.js";
 
 export async function getOrders(req, res) {
   try {
@@ -19,6 +20,7 @@ export async function getOrders(req, res) {
     const hasNextPage = orders.length > limitNum;
     if (hasNextPage) orders.pop();
     const nextCursor = hasNextPage ? orders[orders.length - 1]._id : null;
+    res.status(200).json({ success: true, orders, nextCursor, hasNextPage });
   } catch (error) {
     console.error(error);
     return res.status(500).json({
@@ -40,7 +42,7 @@ export async function deleteOrder(req, res) {
       success: true,
       message: `${order._id} deleted successfully`,
     });
-  } catch {
+  } catch (error) {
     console.error(error);
     return res.status(500).json({
       success: false,
@@ -60,6 +62,69 @@ export async function getMyOrders(req, res) {
       .sort({ createdAt: -1, _id: -1 })
       .limit(limitNum + 1)
       .lean();
+
+    const hasNextPage = orders.length > limitNum;
+    if (hasNextPage) orders.pop();
+    const nextCursor = hasNextPage ? orders[orders.length - 1]._id : null;
+    return res.status(200).json({
+      orders,
+      hasNextPage,
+      nextCursor,
+      success: true,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+}
+
+export async function getOrderById(req, res) {
+  try {
+    const order = await Order.findById(req.params.id);
+    if (!order)
+      return res.status(400).json({
+        success: false,
+        message: "Order not found",
+      });
+    return res.status(200).json({
+      success: true,
+      order,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+}
+export async function guestLookupOrder(req, res) {
+  try {
+    const { email, reference } = req.body;
+    if (!email || !reference)
+      return res.status(400).json({
+        success: false,
+        message: "Email and reference needed",
+      });
+    const order = await Order.findOne({
+      "customer.email": email,
+      "customer.userId": null,
+      paystackReference: reference,
+    });
+
+    if (!order)
+      return res.status(400).json({
+        success: false,
+        message: "Order not found",
+      });
+
+    return res.status(200).json({
+      order,
+      success: true,
+    });
   } catch (error) {
     console.error(error);
     return res.status(500).json({
@@ -83,7 +148,7 @@ export async function createOrder(req, res) {
     const productIds = items.map((item) => item.productId);
 
     const products = await Product.find({
-      _id: { $inc: productIds },
+      _id: { $in: productIds },
       isActive: true,
     });
 
@@ -94,7 +159,7 @@ export async function createOrder(req, res) {
       });
 
     for (const item of items) {
-      const product = products.map((p) => p._id.toString() === item.productId);
+      const product = products.find((p) => p._id.toString() === item.productId);
       if (product.stock < item.quantity)
         return res.status(400).json({
           success: false,
@@ -103,7 +168,7 @@ export async function createOrder(req, res) {
     }
 
     const orderItems = items.map((item) => {
-      const product = products.map((p) => p._id.toString() === item.productId);
+      const product = products.find((p) => p._id.toString() === item.productId);
       return {
         product: product._id,
         name: product.name,
@@ -138,6 +203,44 @@ export async function createOrder(req, res) {
       order,
       success: true,
       message: `${order?._id} created successfully`,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+}
+
+export async function markOrderAsPaid(paystackReference) {
+  const order = await Order.findOneAndUpdate(
+    { paystackReference },
+    { paymentStatus: "paid" },
+  );
+  if (!order) throw new Error("Order not found");
+
+  await Promise.all([
+    order.items?.map(async (item) => {
+      await Product.findByIdAndUpdate(item.product, {
+        $inc: { stock: -item.quantity },
+      });
+      await checkAndDeactivate(item.product);
+    }),
+  ]);
+}
+
+export async function updateDeliveryStatus(req, res) {
+  try {
+    const { deliveryStatus } = req.body;
+    const order = await Order.findByIdAndUpdate(
+      req.params.id,
+      { deliveryStatus },
+      { new: true, runValidators: true },
+    );
+    return res.status(200).json({
+      order,
+      success: true,
     });
   } catch (error) {
     console.error(error);
